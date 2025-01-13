a7d306249d441f8b4376a5d020d31623150b5313
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-13 16:02:17 +0530

    Epi - 08 - Intermediate - Navigate - GO Back

--- src/components/Support.js
+++ src/components/Support.js
@@ -3,14 +3,18 @@ import { useNavigate } from 'react-router-dom'
 
 const Support = () => {
     const navigate = useNavigate();
-    
+
     function clickHandler(){
         navigate("/about");
+    }
+    function backHandler(){
+        navigate(-1);
     }
   return (
     <div>
         <div>Support</div>
         <button onClick={clickHandler}>Move to About Page</button>
+        <button onClick={backHandler}>Go Back</button>
     </div>
   )
 }
