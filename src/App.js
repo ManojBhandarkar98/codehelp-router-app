da106c9a407368f370c166b01393f60797b7e5c7
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-13 12:26:28 +0530

    Epi - 08 - Intermediate - react all route pages created and link to component from home page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Support from './components/Support';
 import About from './components/About';
 import Contact from './components/Contact';
 import NotFound from './components/NotFound';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div className="App">
       Router - App
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/support">Support</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
       <Routes>
         //Multiple Route
-        <Route path='/' element={<div>Home </div>}>Single Route</Route>
-        <Route path='/support' element={<Support/>}>Support</Route>
+        <Route path='/' element={<Home />}>Single Route</Route>
+        <Route path='/support' element={<Support />}>Support</Route>
         <Route path='/about' element={<About />}>About</Route>
-        <Route path='/contact' element={<Contact/>}>Contact Us</Route>
-        <Route path='*' element={<NotFound/>}/>
+        <Route path='/contact' element={<Contact />}>Contact Us</Route>
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   );
