17e014a234a4589a608b59c02a09c706f7738d4d
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-13 12:12:43 +0530

    Epi - 08 - Intermediate - react all route pages created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Support from './components/Support';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
       Router - App
+      <Routes>
+        //Multiple Route
+        <Route path='/' element={<div>Home </div>}>Single Route</Route>
+        <Route path='/support' element={<Support/>}>Support</Route>
+        <Route path='/about' element={<About />}>About</Route>
+        <Route path='/contact' element={<Contact/>}>Contact Us</Route>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
     </div>
   );
 }
